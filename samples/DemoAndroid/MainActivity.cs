48979fb744d3b5131b9fe0231157c16ffd7eb410
Tri Nguyen <tringuyen.nnt@gmail.com>
2018-08-27 16:41:58 +0700

    fix binding libraries with latest Xamarin.Android release

---
+++ samples/DemoAndroid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace DemoAndroid
+{
+    [Activity(Label = "DemoAndroid", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // test
+            PhotoView photoView = new PhotoView(container.Context);
+        }
+    }
+}
+
