c32c028629fae05904084f9a3a357d3a3a52c1ea
Tri Nguyen <tringuyen.nnt@gmail.com>
2020-06-26 15:05:14 +0200

    Migrate to AndroidX

---
+++ samples/DemoAndroidX/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace DemoAndroidX
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
