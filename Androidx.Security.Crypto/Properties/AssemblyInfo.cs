5465e1e378204a2f32439e2838ba2bdcc12cf87b
Tri Nguyen <tnn@trinnguyen.com>
2020-11-07 15:29:18 +0100

    add Xamarin binding for AndroidX crypto

---
+++ Androidx.Security.Crypto/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Androidx.Security.Crypto")]
+[assembly: AssemblyDescription("Xamarin.Android binding for androidx.security:security-crypto:1.1.0-alpha02")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Androidx.Security.Crypto")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.1.0.2")]
+[assembly: AssemblyFileVersion("1.1.0.2")]
