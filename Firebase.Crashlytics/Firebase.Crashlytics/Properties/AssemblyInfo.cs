ec011491fe39662f748e606e055b5505cd1f9a90
Tri Nguyen <tnn@trinnguyen.com>
2020-11-07 15:59:54 +0100

    add Xamarin.Android binding for firebase-crashlytics version 17.1.1

---
+++ Firebase.Crashlytics/Firebase.Crashlytics/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tnn.Firebase.Crashlytics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tnn.Firebase.Crashlytics")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("17.1.1")]
+[assembly: AssemblyFileVersion("17.1.1")]
